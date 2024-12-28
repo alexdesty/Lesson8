678345732d178cb7853c302ad10954437eb938a6
alexdesty <alex.desty@gmail.com>
2024-12-28 20:14:53 +0300

    add homework

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -1,8 +1,27 @@
-﻿namespace Lesson8;
-    internal class Program
+﻿using System.Text;
+
+namespace Lesson8task1;
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter string: ");
+        var userString = new StringBuilder(Console.ReadLine().ToLower());
+        var reversedString = new StringBuilder(userString.ToString());
+        var index = 0;
+        for (var i = userString.Length - 1;i >= 0;i--)
         {
-            Console.WriteLine("Hello, World!");
+            reversedString[index] = userString[i];
+            index++;
+        }
+        var isEquals = String.Compare(userString.ToString(), reversedString.ToString());
+        if (isEquals != 0)
+        {
+            Console.WriteLine("The string isn't a palindrome");
+        }
+        else
+        {
+            Console.WriteLine("The string is a palindrome");
         }
     }
+}
---
+++ Lesson8task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Lesson8task2;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter string: ");
+        var userString = new StringBuilder(Console.ReadLine());
+        Console.WriteLine("String without spaces:");
+        for (int i = 0;i < userString.Length;i++)
+        {
+            if (userString[i] != ' ')
+            {
+                Console.Write(userString[i]);
+            }
+        }
+    }
+}
